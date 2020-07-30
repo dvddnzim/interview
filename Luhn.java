e955a8413ffa873fdb1540fbfdde5f91e2e813df
dvddnzim <69013634+dvddnzim@users.noreply.github.com>
2020-07-30 14:41:36 -0700

    Issue 3 fixes

--- Luhn.java
+++ Luhn.java
@@ -29,10 +29,11 @@ public class Luhn {
 	 * 
 	 * @param cardNumber
 	 *            the card number (not including a check digit)
+         *            Card numbers are generally 16 digits, so this needs to be a long
 	 * 
 	 * @return the check digit
 	 */
-	public int generateCheckDigit(int cardNumber) {
+	public int generateCheckDigit(long cardNumber) {
 		boolean doubleDigit = true;
 		int sum = 0;
 		while (cardNumber > 0) {
@@ -43,7 +44,8 @@ public class Luhn {
 				digit *= 2;
 
 				// if two digits, use the sum of the digits
-				if (digit > 10) {
+                                // NOTE: The number 10 is 2 digits.
+				if (digit >= 10) {
 					digit = digit / 10 + digit % 10; 
 				}
 			}
@@ -66,12 +68,24 @@ public class Luhn {
 	 * 
 	 * @param startRange
 	 *            the starting card number of the range (may not be valid luhn)
+         *            DZIMM: This is ambiguous. What does "may not" mean? That it might
+         *                   not be valid, or that it's not allowed to be valid? If it's
+         *                   valid, what are we supposed to do? Return an error?
 	 * @param endRange
 	 *            the ending card number of the range (may not be a valid luhn)
+         *            Card numbers are generally 16 digits, so these need to be a long
 	 * 
 	 * @return the number of valid Luhn card numbers in the range, inclusive
 	 */
-	public int countRange(int startRange, int endRange) {
-		return 1;
+	public int countRange(long startRange, long endRange) {
+                int numValid = 0;
+                for (long l = startRange; l <= endRange; l++)
+                {
+                  if (this.isValidLuhn(l))
+                  {
+                    numValid++;
+                  }
+                }
+		return numValid;
 	}
 }
--- LuhnUnitTest.java
+++ LuhnUnitTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Ignore;
 
 public class LuhnUnitTest {
 
@@ -19,6 +20,15 @@ public class LuhnUnitTest {
 	@Test
 	public void generateCheckDigit() {
 		assertEquals(9, luhn.generateCheckDigit(92739871));
+		assertEquals(7, luhn.generateCheckDigit(92739872));
+                assertEquals(5, luhn.generateCheckDigit(4959591803L));
+		assertEquals(3, luhn.generateCheckDigit(7992739871L));
+		assertEquals(4, luhn.generateCheckDigit(948417766128L));
+		assertEquals(6, luhn.generateCheckDigit(286635141968L));
+		assertEquals(1, luhn.generateCheckDigit(58021710386316L));
+		assertEquals(6, luhn.generateCheckDigit(93852725542205L));
+		assertEquals(9, luhn.generateCheckDigit(7073964599192709L));
+		assertEquals(2, luhn.generateCheckDigit(3515080212530012L));
 	}
 
 	@Test
@@ -50,7 +60,8 @@ public class LuhnUnitTest {
 
 	@Test
 	public void countRange() {
-		assertEquals(1, 927398710, 927398720);
+		assertEquals(1, luhn.countRange(927398710, 927398720));
+		assertEquals(2, luhn.countRange(927398710, 927398730));
 	}
 
 }
